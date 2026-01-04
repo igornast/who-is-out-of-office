e8c1f4d6a5f28aa6c1258f32625c3c380ee6c976
igornast <igor.nastarowicz@hotmail.com>
2026-01-04 15:50:56 +0100

    feat: mark WeeklyDigestCommand AsCronTask

--- app/src/Infrastructure/Slack/Command/WeeklyDigestCommand.php
+++ app/src/Infrastructure/Slack/Command/WeeklyDigestCommand.php
@@ -9,8 +9,12 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Scheduler\Attribute\AsCronTask;
 
-#[AsCommand(name: 'slack:weekly_digest', description: 'Send weekly digest notification')]
+#[
+    AsCommand(name: 'slack:weekly_digest', description: 'Send weekly digest notification'),
+    AsCronTask(expression: '15 8 * * MON'),
+]
 class WeeklyDigestCommand extends Command
 {
     public function __construct(private readonly SlackFacadeInterface $slackFacade)
